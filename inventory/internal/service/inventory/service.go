826dc80f5830165a2e23219dc86ff67134386b18
Vyacheslav <obvintseff.vyacheslav@yandex.ru>
2025-11-27 16:15:31 +0500

    fix: def for check implements methods
    ref: #2

--- inventory/internal/service/inventory/service.go
+++ inventory/internal/service/inventory/service.go
@@ -1,6 +1,11 @@
 package inventory
 
-import "github.com/clava1096/rocket-service/inventory/internal/repository"
+import (
+	"github.com/clava1096/rocket-service/inventory/internal/repository"
+	def "github.com/clava1096/rocket-service/inventory/internal/service"
+)
+
+var _ def.InventoryService = (*service)(nil)
 
 type service struct {
 	inventoryRepository repository.InventoryRepository

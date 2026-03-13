a76ba0ac01d6ac56d7b1217cbc217a15526487e4
ant1soc1al <obvintseff.vyacheslav@yandex.ru>
2026-03-13 18:42:48 +0500

    fix: integration test
    ref: #4

--- order/internal/repository/order/suite_test.go
+++ order/internal/repository/order/suite_test.go
@@ -2,34 +2,70 @@ package model
 
 import (
 	"context"
-	"log"
 	"testing"
 	"time"
 
 	"github.com/brianvoe/gofakeit/v7"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/stretchr/testify/suite"
-
-	"github.com/clava1096/rocket-service/order/internal/config"
 	"github.com/clava1096/rocket-service/order/internal/model"
+	"github.com/clava1096/rocket-service/platform/pkg/logger"
+	"github.com/clava1096/rocket-service/platform/pkg/testcontainers/postgres"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
+	"github.com/stretchr/testify/suite"
 )
 
 type RepositorySuite struct {
 	suite.Suite
-	repo *repository
-	ctx  context.Context
+
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	pool        *pgxpool.Pool
+	repo        *repository
+	pgContainer *postgres.Container
+}
+
+func (s *RepositorySuite) SetupSuite() {
+	s.ctx, s.cancel = context.WithTimeout(context.Background(), 5*time.Minute)
+
+	pgContainer, err := postgres.NewContainer(s.ctx,
+		postgres.WithImageName("postgres:18.2"),
+		postgres.WithDatabase("test_orders"),
+		postgres.WithAuth("test_user", "test_password"),
+		postgres.WithLogger(logger.Logger()))
+
+	s.NoError(err)
+	s.pgContainer = pgContainer
+
+	uri := pgContainer.URI()
+	s.pool, err = pgxpool.New(s.ctx, uri)
+	s.NoError(err)
+
+	sqlDB := stdlib.OpenDB(*s.pool.Config().ConnConfig)
+	defer sqlDB.Close()
+
+	err = goose.Up(sqlDB, "../../../migrations")
+	s.NoError(err, "failed to apply migrations")
+
+	s.repo = NewRepository(s.pool)
 }
 
 func (s *RepositorySuite) SetupTest() {
-	s.ctx = context.Background()
+	_, err := s.pool.Exec(s.ctx, "TRUNCATE TABLE orders RESTART IDENTITY CASCADE")
+	s.NoError(err)
+}
 
-	cfg, err := config.GetConfig()
-	if err != nil {
-		log.Fatal("Error loading config...")
+func (s *RepositorySuite) TearDownSuite() {
+	if s.pool != nil {
+		s.pool.Close()
 	}
-	pool, err := pgxpool.New(s.ctx, cfg.Postgres.GetPostgresUri())
-
-	s.repo = NewRepository(pool)
+	if s.pgContainer != nil {
+		_ = s.pgContainer.Terminate(s.ctx)
+	}
+	if s.cancel != nil {
+		s.cancel()
+	}
 }
 
 func TestRepository(t *testing.T) {

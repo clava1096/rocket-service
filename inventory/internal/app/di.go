d5bcd195ef01bfe3d9c932b18dfd08031b573ee7
ant1soc1al <obvintseff.vyacheslav@yandex.ru>
2026-03-13 18:40:45 +0500

    fix: di container in inventory
    ref: #4

--- inventory/internal/app/di.go
+++ inventory/internal/app/di.go
@@ -2,11 +2,15 @@ package app
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/clava1096/rocket-service/inventory/internal/config"
 	inventoryRepository "github.com/clava1096/rocket-service/inventory/internal/repository/part"
 	inventoryService "github.com/clava1096/rocket-service/inventory/internal/service/inventory"
+	"github.com/clava1096/rocket-service/platform/pkg/closer"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 
 	inventoryAPI "github.com/clava1096/rocket-service/inventory/internal/api/inventory/v1"
 	"github.com/clava1096/rocket-service/inventory/internal/repository"
@@ -51,18 +55,32 @@ func (d *diContainer) PartRepository(ctx context.Context) repository.PartReposit
 	return d.inventoryRepository
 }
 
+func (d *diContainer) MongoDBClient(ctx context.Context) *mongo.Client {
+	if d.mongoDBClient == nil {
+		client, err := mongo.Connect(ctx, options.Client().ApplyURI(config.AppConfig().MongoDb.URI()))
+
+		if err != nil {
+			panic(fmt.Sprintf("failed to connect to MongoDB: %s\n", err.Error()))
+		}
+		err = client.Ping(ctx, readpref.Primary())
+
+		if err != nil {
+			panic(fmt.Sprintf("failed to ping MongoDB: %v\n", err))
+		}
+
+		closer.AddNamed("MongoDB client", func(ctx context.Context) error {
+			return client.Disconnect(ctx)
+		})
+		d.mongoDBClient = client
+	}
+
+	return d.mongoDBClient
+}
+
 func (d *diContainer) MongoDBHandle(ctx context.Context) *mongo.Database {
 	if d.mongoDBHandle == nil {
 		d.mongoDBHandle = d.MongoDBClient(ctx).Database(config.AppConfig().MongoDb.DatabaseName())
 	}
 
 	return d.mongoDBHandle
-}
-
-func (d *diContainer) MongoDBClient(ctx context.Context) *mongo.Client {
-	if d.mongoDBClient == nil {
-		d.mongoDBClient = d.MongoDBClient(ctx)
-	}
-
-	return d.mongoDBClient
 }
